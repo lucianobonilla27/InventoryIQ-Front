a3a0d5d160fc0f171f09fbd2ad64cd24485482c5
facundo <facurmedina@gmail.com>
2024-04-07 21:41:36 -0300

    se agrego el usuario admin predeterminado y se agregó una función que si se elimina el usuario admin que está logueado, se cierre la sesión y se redirija al home

--- InventoryIQ/src/context/UsuariosContext.jsx
+++ InventoryIQ/src/context/UsuariosContext.jsx
@@ -1,5 +1,6 @@
 import { children, createContext, useEffect, useState } from "react";
 import axios from "axios";
+import { unstable_batchedUpdates } from "react-dom";
 
 export const UsuariosProvider = createContext()
 
@@ -29,6 +30,7 @@ const UsuariosContext = ({children}) => {
         } catch (error) {
             console.log(error);
         }
+
     }
 
 
@@ -45,6 +47,13 @@ const UsuariosContext = ({children}) => {
         try {
             await axios.delete(`http://localhost:7000/usuarios/${id}`)
             setUsuarios(usuarios.filter((usuario) => usuario.id !==id));
+
+            const usuarioActual = JSON.parse(localStorage.getItem("user"));
+            if (usuarioActual && usuarioActual.id === id) {
+                logout();
+            }
+
+            setUsuarios(usuarios.filter((usuario) => usuario.id !== id));
         } catch (error) {
             console.log(error)
         }
@@ -58,11 +67,39 @@ const UsuariosContext = ({children}) => {
     }
 
 
-    useEffect(() => {
-        getUsers()
-    }
-    , [])
+    // useEffect(() => {
+            
 
+    //     getUsers()
+    // }
+    // , [])
+
+    useEffect(() => {
+        const addDefaultUser = async () => {
+            try {
+                //ver si el usuario existe
+                const existeUsuario = await axios.get(`http://localhost:7000/usuarios`);
+                const existeUsuarioPredeterminado = existeUsuario.data.some(user => user.id === 1);
+                //si no existe, se agrega el usuario predeterminado
+                if (!existeUsuarioPredeterminado) {
+                    await addUser({
+                        id: 1, 
+                        nombre: 'Admin' ,
+                        email: 'admin@admin.com', 
+                        password: 'Admin159@', 
+                        confirmPassword: 'Admin159@', 
+                        isAdmin: true
+                    });
+                    
+                }
+            } catch (error) {
+                console.log('Error al cargar el usuario predeterminado', error)
+                
+            };
+        };
+        addDefaultUser();
+        getUsers();
+    }, []);
 
     return (
         <UsuariosProvider.Provider value={{ usuarios, getUsers, addUser, logout, editarUsuario, deleteUsuarios }}>
