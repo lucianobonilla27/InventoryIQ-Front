aa792d640799b76023731b53cd3ff59ada715c52
Claudio Joaquin Armando <armandoclaudio1995@gmail.com>
2024-04-20 15:48:09 -0300

    eliminacion de archivos innecesarios y mejora estetia de about us

--- InventoryIQ/view/about/About.jsx
+++ InventoryIQ/view/about/About.jsx
@@ -7,98 +7,93 @@ const About = () => {
   return (
     <>
     <div className="container-fluid">
-      <h2 className="text-center mb-4">About Us</h2>
+      <h2 className="text-center my-3">Quienes somos</h2>
 
-      {/* Carrusel */}
-      <Carousel>
+      <Carousel >
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="path_to_image1.jpg"
+            src="https://mecaluxar.cdnwm.com/blog/img/control-de-stock-deposito.1.1.jpg"
             alt="First slide"
           />
-          <Carousel.Caption>
-            <h3>Slide 1</h3>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="path_to_image2.jpg"
+            src="https://www.il-latam.com/wp-content/uploads/2023/06/Control-del-Inventario-vs.-Optimizacion-del-Inventario-e1687813689818.jpg"
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://cms.ar-racking.com/uploads/2021/03/tipos-stock-almacen-1.jpg"
             alt="Second slide"
           />
-          <Carousel.Caption>
-            <h3>Slide 2</h3>
-          </Carousel.Caption>
         </Carousel.Item>
-        {/* Agregar más slides según necesites */}
       </Carousel>
 
-      {/* Descripción */}
       <div className="mt-5">
-        <h3><FaHistory /> Our History</h3>
+        <h3><FaHistory /> Nuestra Historia</h3>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec commodo, neque non tempus aliquet, lectus ex dapibus risus, vel consectetur odio nisl id erat.
+          Todo empezó con una idea simple pero poderosa: ayudar a los comerciantes locales a gestionar mejor su inventario y a maximizar sus ventas.
         </p>
-
-        <h3><FaBullseye /> Our Mission</h3>
+        <p>Nuestro fundador, Luciano, había trabajado en el sector del comercio durante años y había experimentado de primera mano los desafíos de mantener un control eficiente del stock. decidió crear una solución innovadora: un sistema de control de stock fácil de usar, flexible y adaptado a las necesidades de los pequeños y medianos comerciantes.</p>
+        <p>Con el apoyo de un pequeño equipo de desarrolladores y expertos en negocios, lanzamos nuestra primera versión del software de control de stock. A medida que los comerciantes comenzaron a utilizar nuestra plataforma, nos dimos cuenta de que estábamos en algo grande. Con el tiempo, hemos crecido y evolucionado, pero nuestra misión sigue siendo la misma: ofrecer herramientas poderosas y fáciles de usar que permitan a los comerciantes gestionar su inventario de manera eficiente, aumentar sus ventas y hacer crecer sus negocios. Hoy en día, estamos orgullosos de haber ayudado a miles de comerciantes a tener éxito.</p>
+        <h3><FaBullseye /> Nuestra mision</h3>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec commodo, neque non tempus aliquet, lectus ex dapibus risus, vel consectetur odio nisl id erat.
+        Facilitar el éxito de los comerciantes ofreciendo un control de stock inteligente, intuitivo y personalizado que les permita optimizar sus operaciones, aumentar sus ventas y crecer de manera sostenible.
         </p>
       </div>
 
-      {/* Equipo */}
       <div className="mt-5">
-        <h3>Our Team</h3>
+        <h3 className="row justify-content-center mt-4">Nuestro equipo</h3>
         <div className="row justify-content-center mt-4">
-          {/* Card para cada integrante */}
           <div className="col-md-3 col-sm-5 mb-4">
             <div className="card">
-              <img src="path_to_member1.jpg" className="card-img-top" alt="Member 1" />
+              <img src="https://cdn-icons-png.flaticon.com/512/3135/3135768.png" className="card-img-top" alt="Member 1" />
               <div className="card-body">
-                <h5 className="card-title">Member 1</h5>
+                <h5 className="card-title text-truncate">Luciano Bonilla</h5>
                 <p className="card-text">Position</p>
               </div>
             </div>
           </div>
           <div className="col-md-3 col-sm-5 mb-4">
             <div className="card">
-              <img src="path_to_member1.jpg" className="card-img-top" alt="Member 1" />
+              <img src="https://cdn-icons-png.flaticon.com/512/3135/3135768.png" className="card-img-top" alt="Member 1" />
               <div className="card-body">
-                <h5 className="card-title">Member 1</h5>
+                <h5 className="card-title text-truncate">Lorena Oterino</h5>
                 <p className="card-text">Position</p>
               </div>
             </div>
           </div>
           <div className="col-md-3 col-sm-5 mb-4">
             <div className="card">
-              <img src="path_to_member1.jpg" className="card-img-top" alt="Member 1" />
+              <img src="https://cdn-icons-png.flaticon.com/512/3135/3135768.png" className="card-img-top" alt="Member 1" />
               <div className="card-body">
-                <h5 className="card-title">Member 1</h5>
+                <h5 className="card-title text-truncate">Facundo Medina</h5>
                 <p className="card-text">Position</p>
               </div>
             </div>
           </div>
           <div className="col-md-3 col-sm-5 mb-4">
             <div className="card">
-              <img src="path_to_member1.jpg" className="card-img-top" alt="Member 1" />
+              <img src="https://cdn-icons-png.flaticon.com/512/3135/3135768.png" className="card-img-top" alt="Member 1" />
               <div className="card-body">
-                <h5 className="card-title">Member 1</h5>
+                <h5 className="card-title text-truncate">Monica Ruiz</h5>
                 <p className="card-text">Position</p>
               </div>
             </div>
           </div>
           <div className="col-md-3 col-sm-5 mb-4">
             <div className="card">
-              <img src="path_to_member1.jpg" className="card-img-top" alt="Member 1" />
+              <img src="https://cdn-icons-png.flaticon.com/512/3135/3135768.png" className="card-img-top" alt="Member 1" />
               <div className="card-body">
-                <h5 className="card-title">Member 1</h5>
+                <h5 className="card-title text-truncate">Claudio Armando</h5>
                 <p className="card-text">Position</p>
               </div>
             </div>
           </div>
-
-          {/* Agregar más cards para otros integrantes */}
         </div>
       </div>
     </div>
